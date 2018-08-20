3bd7c0cd25a4fad43d89990b123adf8741ef7289
hbk <540243427@qq.com>
2018-08-20 21:58:09 +0800

    优化页面，添加左右滑动翻页功能

--- src/main/resources/static/xxgg/common.js
+++ src/main/resources/static/xxgg/common.js
@@ -55,6 +55,68 @@ function topCase(){
 
 function footer(){
     document.getElementsByClassName("p")[0].getElementsByTagName("a")[0].innerHTML="首页";
+    var page = document.getElementsByClassName("page_chapter")[0];
+    var title = document.getElementsByClassName("content")[0].getElementsByTagName("h1")[0];
+    title.parentNode.insertBefore(page.cloneNode(true),title.nextSibling );
+    var returnTop = document.createElement("li");
+    var a = document.createElement("a");
+    a.setAttribute("href","#top");
+    a.innerText = "返回顶部";
+    returnTop.insertBefore(a,null);
+    page.getElementsByTagName("ul")[0].insertBefore(returnTop,page.getElementsByTagName("ul")[0].firstChild);
+
+    //添加左右滑动翻页
+    var ele = document.getElementById("wrapper");
+    var beginX, beginY, endX, endY, swipeLeft, swipeRight;
+    ele.addEventListener('touchstart', function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        beginX = event.targetTouches[0].screenX;
+        beginY = event.targetTouches[0].screenY;
+        swipeLeft = false, swipeRight = false;
+    });
+
+    ele.addEventListener('touchmove', function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        endX = event.targetTouches[0].screenX;
+        endY = event.targetTouches[0].screenY;
+        // 左右滑动
+        if (Math.abs(endX - beginX) - Math.abs(endY - beginY) > 0) {
+            /*向右滑动*/
+            if (endX - beginX > 0) {
+                swipeRight = true;
+                swipeLeft = false;
+            }
+            /*向左滑动*/
+            else {
+                swipeLeft = true;
+                swipeRight = false;
+            }
+        }
+        else if(Math.abs(endX - beginX) - Math.abs(endY - beginY) < 0)
+        {
+            // 上下滑动
+        }
+    });
+    ele.addEventListener('touchend', function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (Math.abs(endX - beginX) - Math.abs(endY - beginY) > 0) {
+            event.stopPropagation();
+            event.preventDefault();if (swipeRight) {
+                swipeRight = !swipeRight;
+                /*向右滑动*/
+                location.href=document.getElementsByClassName("page_chapter")[0].getElementsByTagName("a")[0].href;
+            }
+            if(swipeLeft) {
+                swipeLeft = !swipeLeft;
+                /*向左滑动*/
+                location.href=document.getElementsByClassName("page_chapter")[0].getElementsByTagName("a")[2].href;
+            }
+        }
+    });
 }
 
 document.writeln("<script src=\'/xxgg/jquery.min.js'></script>");
